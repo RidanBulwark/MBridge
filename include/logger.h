d786f55f2038431152ebb801d7a82cf0f58d29c9
RidanBulwark <pragaibotond@gmail.com>
2026-06-23 09:46:29 +0200

    fix: Introduce memory safety

--- include/logger.h
+++ include/logger.h
@@ -1,7 +1,8 @@
+#pragma once
 #define DEBUG_LEVEL 1  // 0 = Silent, 1 = Production, 2 = Verbose Dev
 
 #if DEBUG_LEVEL > 0
-    void APP_LOG(const char *format, ...);
+    void APP_LOG(const char *format, ...) __attribute__ ((format (printf, 1, 2)));
     void APP_LOG_Init(void);
 #else
     // Compiles to literally zero CPU instructions when you build for customer release
